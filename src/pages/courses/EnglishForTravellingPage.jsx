9b2c444c4718149888388f84f7df419488054469
Abazur24 <zurayev@hotmail.co.uk>
2024-10-16 14:54:38 +0200

    popup contact form added

--- src/pages/courses/EnglishForTravellingPage.jsx
+++ src/pages/courses/EnglishForTravellingPage.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 import './coursesPages.scss';
 import travellingImage from '../../assets/images/travelling.png';
+import ContactFormModal from '../../components/ContactFormModal'; // Import the modal
 
 const EnglishForTravellingPage = () => {
+  const [isModalOpen, setModalOpen] = useState(false); // Manage modal state
+
+  const handleOpenModal = () => {
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
+
   return (
     <div className="course-container travelling-english">
       <div className="content-wrapper">
@@ -12,12 +22,15 @@ const EnglishForTravellingPage = () => {
           <p className="description">
             Our English for Travelling Course is designed for globetrotters looking to master essential communication skills needed during travel.
           </p>
-          <Link to="/english-for-travelling" className="contact-button-pages">Contact Our Manager</Link>
+          <button onClick={handleOpenModal} className="contact-button-pages">Contact Our Manager</button>
         </div>
         <div className="image-section">
           <img className="course-image" src={travellingImage} alt="English for Travelling" />
         </div>
       </div>
+
+      {/* Contact Form Modal */}
+      <ContactFormModal isOpen={isModalOpen} onClose={handleCloseModal} />
     </div>
   );
 };
