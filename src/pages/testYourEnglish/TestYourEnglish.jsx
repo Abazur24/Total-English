ab9974278073b2893517e5b62a88adf46c170572
Abazur24 <zurayev@hotmail.co.uk>
2024-10-24 17:16:20 +0200

    English Test Page updated

--- src/pages/testYourEnglish/TestYourEnglish.jsx
+++ src/pages/testYourEnglish/TestYourEnglish.jsx
@@ -10,9 +10,7 @@ const TestYourEnglish = () => {
         <div className="content-wrapper">
           <h1>Test your English Level</h1>
           <p>
-            Assess your English proficiency with the University of Cambridge Level Test! Whether you're just starting or already fluent, 
-            it's essential to know where you stand. Our test provides insights into your grammar, vocabulary, 
-            and overall language abilities.
+          Assess your English proficiency with the University of Cambridge Level Test! Whether you're just starting or already fluent, it's essential to know where you stand. Our test provides insights into your grammar, vocabulary, and overall language abilities. Once you’ve completed the test, return to our portal and contact one of our managers for personalized guidance and next steps to continue your learning journey.
           </p>
           <a 
             href="https://www.cambridgeenglish.org/test-your-english/general-english/" 
